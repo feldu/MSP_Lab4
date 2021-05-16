53e3214bb8a573c60f4081c7fbc204a68749eb37
raineduc <hramil0120@gmail.com>
2021-05-16 19:18:24 +0300

    Add jmx notification

--- backend/src/main/java/lab4/mbeans/PointsCounter.java
+++ backend/src/main/java/lab4/mbeans/PointsCounter.java
@@ -5,12 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Component;
 
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
 import java.util.List;
 
 @ManagedResource
 @Component
 @Slf4j
-public class PointsCounter implements PointsCounterMBean {
+public class PointsCounter extends NotificationBroadcasterSupport implements PointsCounterMBean {
+    private int sequenceCounter = 0;
     long pointsCnt = 0;
     long missCnt = 0;
 
@@ -18,6 +21,15 @@ public class PointsCounter implements PointsCounterMBean {
         missCnt = points.stream().filter(point -> point.getResult().equals("Не попала")).count();
         pointsCnt = points.size();
         log.info("Counters updated. {} misses from {} points", missCnt, pointsCnt);
+        if (pointsCnt % 5 == 0) {
+            Notification notification = new Notification(
+                    "pointsCountIsMultipleOfFive", this, sequenceCounter++,
+                    System.currentTimeMillis(), "Points count is multiple of five"
+            );
+            this.sendNotification(notification);
+            log.info("pointsCountIsMultipleOfFive id: " + sequenceCounter);
+        }
+        log.error(missCnt + " " + pointsCnt);
     }
 
     @Override
